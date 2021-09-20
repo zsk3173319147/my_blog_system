6775efd5c85f4bd7d9151f8fd92388f7335194c9
zsk <3173319147@qq.com>
2021-09-21 00:30:48 +0800

    no problem

--- blog/urls.py
+++ blog/urls.py
@@ -6,12 +6,12 @@ from . import views
 app_name='blog'
 
 urlpatterns=[
-    path('',views.index,name='index'),
+    path('',views.IndexView.as_view(),name='index'),
     path('full.width',views.full_width,name='full_width'),
     path('about/',views.about,name='about'),
     path('contact',views.contact,name='contact'),
-    path('posts/<int:pk>/',views.detail,name='detail'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
     path('archives/<int:year>/<int:month>/',views.archive,name='archive'),
-    path('categories/<int:pk>/',views.category,name='category'),
+    path('categories/<int:pk>/',views.CategoryView.as_view(),name='category'),
     path('tags/<int:pk>/',views.tag,name='tag')
     ]
